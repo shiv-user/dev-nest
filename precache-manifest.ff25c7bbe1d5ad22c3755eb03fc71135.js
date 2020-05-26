443ca2a14da9f23be4d7751a957b8b9596f1d6a6
shiv-user <shivranjan1497@gmail.com>
2020-05-26 22:16:12 +0530

    Updates

---
+++ precache-manifest.ff25c7bbe1d5ad22c3755eb03fc71135.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1d019a694abacefa1d25c1e042bf5b",
+    "url": "/dev-nest/index.html"
+  },
+  {
+    "revision": "0d69bcf35d8463bcf233",
+    "url": "/dev-nest/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d2262db3087865d8fb6b",
+    "url": "/dev-nest/static/css/main.d7313b1d.chunk.css"
+  },
+  {
+    "revision": "0d69bcf35d8463bcf233",
+    "url": "/dev-nest/static/js/2.d3a02d7e.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/dev-nest/static/js/2.d3a02d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2262db3087865d8fb6b",
+    "url": "/dev-nest/static/js/main.587c9e32.chunk.js"
+  },
+  {
+    "revision": "e17a4ab00e17cd45307b",
+    "url": "/dev-nest/static/js/runtime-main.6fb8ac13.js"
+  },
+  {
+    "revision": "4a597ecb547c3ce071a7bf2475ff074e",
+    "url": "/dev-nest/static/media/background2.4a597ecb.jpg"
+  },
+  {
+    "revision": "e9179d8b44b59c69469496993abf88e5",
+    "url": "/dev-nest/static/media/datascience.e9179d8b.webp"
+  },
+  {
+    "revision": "185fdd3811fb84f09f50bb6694e976e1",
+    "url": "/dev-nest/static/media/fullstack.185fdd38.jpg"
+  },
+  {
+    "revision": "2e4f361507b359998e3ba7c4bd1c2a46",
+    "url": "/dev-nest/static/media/ios.2e4f3615.jpg"
+  },
+  {
+    "revision": "007e2fdd5db8a3c4fbf37ea97508632e",
+    "url": "/dev-nest/static/media/logo – 1.007e2fdd.png"
+  },
+  {
+    "revision": "217eb2875ccea556f81214cdcbd38865",
+    "url": "/dev-nest/static/media/rowImage.217eb287.jpg"
+  }
+]);
